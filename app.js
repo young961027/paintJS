288102602cc7580af32662d25e08f2d311abc84e
loopbackseal <young961027@gmail.com>
2021-01-30 20:19:17 +0900

    paint lines by canvas context

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+canvas.width = 500 ;
+canvas.height = 500;
+
 let painting = false;
 
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x,y);
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else {
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 function stopPainting() {
     painting = false;
+}
+
+function startPainting(event) {
+    painting = true;
 }
 
 function onMouseDown(event) {
@@ -21,7 +39,7 @@ function onMouseUp(event) {
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", onMouseUp);
     canvas.addEventListener("mouseleave", stopPainting);
 }
